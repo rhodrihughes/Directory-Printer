be61b1ce6578c14034688812c3eaa9a7b1bd361d
rhodrihughes <77209254+rhodrihughes@users.noreply.github.com>
2026-02-17 20:13:37 +0000

    Initial Commit

---
+++ Directory Printer/Directory_PrinterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Directory_PrinterApp.swift
+//  Directory Printer
+//
+//  Created by Rhodri on 17/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct Directory_PrinterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
